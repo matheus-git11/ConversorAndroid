7bb0571fc7fdf0ad91ee5c4320e2b087e4474b3b
Matheus Albuquerque <81053209+matheus-git11@users.noreply.github.com>
2022-04-29 09:05:38 -0300

    Add files via upload
    sessão 4

---
+++ ProjetoData/app/src/main/java/com/example/projetodata/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.projetodata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.projetodata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{calculateTip()}
+    }
+
+    fun calculateTip(){
+        val stringInTextField = binding.costOfService.text
+    }
+}
